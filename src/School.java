b90655942351fbdadde0a2b0841c4c9dd0a86008
Brandon Berney <brandonberney@protonmail.com>
2016-03-08 10:14:18 -0500

    add the school class

--- src/School.java
+++ src/School.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
 public class School {
+    private ArrayList<Student> students = new ArrayList<>();
+    private ArrayList<Teacher> teachers = new ArrayList<>();
+
+    public School(ArrayList<Student> studentList, ArrayList<Teacher> teacherList) {
+        students = studentList;
+        teachers = teacherList;
+    }
+
+    public String getGradeLevel(int gradeLevel) {
+        String all = "";
+
+        for (Student obj: students) {
+            if(obj.getLevel() == gradeLevel) {
+                all = all + obj.toString() + "\n";
+            }
+        }
+
+        return all;
+    }
+
+    public String toString() {
+        String all = "";
+        all = all + "Faculty:\n";
+
+        for (Teacher obj: teachers) {
+            all = all + obj.toString() + "\n";
+        }
+
+        all = all + "Student Body:\n";
+        for (Student obj: students) {
+            all = all + obj.toString() + "\n";
+        }
+
+        return all;
+    }
 }
