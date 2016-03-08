8964cdacd5669892c4d334864d55ce4f0df07362
Brandon Berney <brandonberney@protonmail.com>
2016-03-08 10:05:20 -0500

    add the teacher class

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,12 @@
 public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String fName, String lName, String teacherSubject) {
+        super(fName, lName);
+        subject = teacherSubject;
+    }
+
+    public String toString() {
+        return super.toString() + String.format("%n   Subject: %s", subject);
+    }
 }
