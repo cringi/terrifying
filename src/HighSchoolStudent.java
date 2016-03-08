f05f05713575a72370e594617ea0d861c7054c79
Brandon Berney <brandonberney@protonmail.com>
2016-03-08 10:01:44 -0500

    complete the highschoolstudent class

--- src/HighSchoolStudent.java
+++ src/HighSchoolStudent.java
@@ -1,2 +1,17 @@
 public class HighSchoolStudent extends Student {
+    double gpa;
+
+    public HighSchoolStudent(String fName, String lName, int gLevel, double studentGpa) {
+        super(fName, lName, gLevel);
+        if (studentGpa < 0 || studentGpa > 5) {
+            gpa = 0;
+        }
+        else {
+            gpa = studentGpa;
+        }
+    }
+
+    public String toString() {
+        return super.toString() + String.format("%n   GPA: %.1f", gpa);
+    }
 }
