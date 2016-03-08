45ace4453a6ee9ebd8802bbfdf78bdbe68ddd3b0
Brandon Berney <brandonberney@protonmail.com>
2016-03-08 09:58:55 -0500

    complete the student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,22 @@
 public class Student extends Person {
+    private static int studentId;
+    private int level;
+
+    public Student(String fName, String lName, int gLevel) {
+        super(fName, lName);
+        if(gLevel < 0 && gLevel > 12) {
+            level = 0;
+        }
+        else {
+            level = gLevel;
+        }
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String toString() {
+        return super.toString() + String.format("%n   Grade level: %d%n   ID #: %d", level, studentId);
+    }
 }
